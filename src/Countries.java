77c38f3c8a0c061a6846abe48b38048d657a4c85
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-16 08:13:41 -0400

    commit for submission

--- src/Countries.java
+++ src/Countries.java
@@ -1,5 +1,66 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Countries {
     public static void main(String[] args) {
+        HashMap<String, ArrayList<Country>> countries = new HashMap(); //where the key is the first letter of the country name.
+        String fileContent = readFile("countries.txt"); //file is String
+        String[] countriesContent = fileContent.split("\n"); //turn file into String[]?
+
+        for (String countryName : countriesContent) {
+            String firstLetter = String.valueOf(countryName.charAt(0));
+            ArrayList<Country> list = countries.get(firstLetter);
+            String[] column = countryName.split("\\|");
+            String abbrev = column[0];
+            String name = column[1];
+            Country country = new Country(name, abbrev);
+            if (list == null) {
+                list = new ArrayList();
+                list.add(country);
+                countries.put(firstLetter, list);
+            } else {
+                list.add(country);
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type a letter to look up a Country name.");
+        String letter = scanner.nextLine();
+        ArrayList<Country> letterList = countries.get(letter);
+        String contents = "";
+        for (Country names : letterList) {
+            contents += names.name + "\n";
+        }
 
     }
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    static void writeFile(String fileName, String fileContent) {
+        File f = new File(fileName);
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    //Save an "X_countries.txt" file, where X is the letter they typed, which only lists the countries starting with that letter
 }
---
+++ src/Country.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jessicahuffstutler on 10/15/15.
+ */
+public class Country {
+    String name;
+    String abbrev;
+
+    public Country(String name, String abbrev) {
+        this.name = name;
+        this.abbrev = abbrev;
+    }
+}
